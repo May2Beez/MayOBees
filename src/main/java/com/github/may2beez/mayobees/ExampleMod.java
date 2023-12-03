ad0f9e541d9949dfc414d5da7620379d32f1b7c2
GitHub Action <action@github.com>
2023-12-03 12:21:14 +0000

    Initialize template repository proper
    Set package name to com.github.may2beez.mayobees
    Set project name to MayOBees
    Set modid to mayobees

--- src/main/java/com/github/may2beez/mayobees/ExampleMod.java
+++ src/main/java/com/github/may2beez/mayobees/ExampleMod.java
@@ -1,10 +1,10 @@
-package com.example;
+package com.github.may2beez.mayobees;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "mayobees", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
--- src/main/java/com/github/may2beez/mayobees/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/may2beez/mayobees/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.may2beez.mayobees.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
